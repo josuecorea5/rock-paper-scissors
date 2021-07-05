038fe5a6911bcbcf4f139d1fd555e793fb810748
DCoreas <josuepaiz487@gmail.com>
2021-07-05 17:03:05 -0600

    adding to the game to make it work using js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
-const buttons = document.querySelectorAll('.btn-circle');
+const buttons = document.querySelectorAll('.pick');
 const scoreElement = document.getElementById('score')
+const main = document.getElementById('main');
+const selection = document.getElementById('selection');
+const reset = document.getElementById('reset');
+const userSelect = document.getElementById('user-select');
+const computerSelect = document.getElementById('computer-select');
+const result = document.getElementById('win');
 const choises = ['paper', 'rock', 'scissors'];
 
 let score = 0;
@@ -7,27 +13,41 @@ let userChoise = undefined;
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    userChoise = button.getAttribute('data-choise')
+    userChoise = button.getAttribute('data-choise');
     checkWinner();
   })
 })
 
+reset.addEventListener('click', () => {
+  main.style.display = 'flex';
+  selection.style.display = 'none';
+})
+
+
+
 const checkWinner = () => {
   const computerChoise = randomChoise();
+  //update the view when user choise
+  updateSelection(userSelect,userChoise);
+  updateSelection(computerSelect,computerChoise)
   if(userChoise === computerChoise) {
     // empate
+    result.innerText = 'DRAW'
     console.log('empate')
   }else if (userChoise === 'paper' && computerChoise==='rock'
   || userChoise === 'rock' && computerChoise === 'scissors'
   || userChoise === 'scissors' && computerChoise === 'paper'){
     // gana jugador
     updateScore(1)
-    console.log('winner')
+    result.innerText = 'WIN';
   }else {
     // gana maquina
     updateScore(-1)
-    console.log('OAAAAA Perdiste paaa')
+    result.innerText = 'LOST';
   }
+
+  main.style.display = 'none';
+  selection.style.display = 'flex';
 }
 
 const updateScore = (value) => {
@@ -37,4 +57,17 @@ const updateScore = (value) => {
 
 const randomChoise = () => {
   return choises[Math.floor(Math.random() * choises.length)];
+}
+
+const updateSelection = (selectionEl,choise) => {
+  //class reset to options
+  selectionEl.classList.remove('btn-paper');
+  selectionEl.classList.remove('btn-rock');
+  selectionEl.classList.remove('btn-scissors');
+
+  //update the img for the btn
+  const img = selectionEl.querySelector('img');
+  selectionEl.classList.add(`btn-${choise}`);
+  img.src = `./images/icon-${choise}.svg`;
+  img.alt = choise;
 }
